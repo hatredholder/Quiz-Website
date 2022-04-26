ffb36c461bf5389f1772407c7a5c2ca75032876e
hatredholder <the.great.justice@bk.ru>
2022-04-26 19:51:41 +0300

    removed unnecessary imports

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.forms import ModelForm
 
 
 class MyUserCreationForm(UserCreationForm):
