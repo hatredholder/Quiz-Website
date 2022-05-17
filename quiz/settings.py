fb6c6b848b1178145c005110e1a00a924354a63d
hatredholder <the.great.justice@bk.ru>
2022-05-17 20:16:53 +0300

    reformated code

--- quiz/settings.py
+++ quiz/settings.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Added Apps
+    # Apps
     'quizes',
     'questions',
     'results',
@@ -112,7 +111,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_URL = '/static/'
+
+STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [
     BASE_DIR / 'quizes' / 'static',
 ]
